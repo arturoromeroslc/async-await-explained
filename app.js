b60271a6ba62529bcec25249b35bb35384690c66
Arturo Romero <arturo@Arturos-MacBook-Pro.local>
2017-10-29 14:21:41 -0700

    added a second promise

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const appContainer = document.getElementById('app')
 const asyncAPICall = (url) => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
@@ -8,12 +9,33 @@ const asyncAPICall = (url) => {
   })
 }
 
+//single Promise
 asyncAPICall('https://swapi.co/api/people/1/')
   .then(data => {
     console.log(data);
-    document.getElementById('app').innerHTML = `
+    appContainer.innerHTML = `
       NAME: <br /><strong>${data.name}</strong><br />
       Birth Year: <br /><strong>${data.birth_year}</strong>
     `
   })
   .catch(err => console.log(err))
+
+/*
+composing Promises
+1. Makes an HTTP call, waits for it to complete, and prints the result;
+2. then makes other two parallel HTTP calls;
+3. When both of them complete, prints their result.
+ */
+
+ const callFirstPromise = asyncAPICall('https://swapi.co/api/people/2/')
+
+ callFirstPromise.then((data) => {
+   console.log(data);
+   const createdElement = document.createElement('div');
+   createdElement.innerHTML = `
+   NAME: <br /><strong>${data.name}</strong><br />
+   Birth Year: <br /><strong>${data.birth_year}</strong>`
+   appContainer.appendChild(createdElement)
+   console.log(appContainer);
+   console.log(createdElement);
+ })
