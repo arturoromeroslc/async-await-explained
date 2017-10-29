45e0f67d3e16dbb31b2bd057cf3b3d57f834a76f
Arturo Romero <arturo@Arturos-MacBook-Pro.local>
2017-10-28 23:57:24 -0700

    quick code update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-function myAsyncFunction(url) {
+const asyncAPICall = (url) => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
+    xhr.open('GET', url);
     xhr.onload = () => resolve(xhr.responseText);
     xhr.onerror = () => reject(xhr.statusText);
     xhr.send();
-  });
+  })
 }
 
-myAsyncFunction('https://swapi.co/api/people/1/').then(function (data) {
+asyncAPICall('https://swapi.co/api/people/1/').then(function (data) {
   console.log(data);
 })
