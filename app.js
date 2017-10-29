0a55fdf4db31eab4eb24ba1846d22018cf69aaf2
Arturo Romero <arturo@Arturos-MacBook-Pro.local>
2017-10-29 14:09:54 -0700

    add values to the page

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const asyncAPICall = (url) => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
-    xhr.onload = () => resolve(xhr.responseText);
+    xhr.onload = () => resolve(JSON.parse(xhr.responseText));
     xhr.onerror = () => reject(xhr.statusText);
     xhr.send();
   })
 }
 
-asyncAPICall('https://swapi.co/api/people/1/').then(function (data) {
-  console.log(data);
-})
+asyncAPICall('https://swapi.co/api/people/1/')
+  .then(data => {
+    console.log(data);
+    document.getElementById('app').innerHTML = `
+      NAME: <br /><strong>${data.name}</strong><br />
+      Birth Year: <br /><strong>${data.birth_year}</strong>
+    `
+  })
+  .catch(err => console.log(err))
